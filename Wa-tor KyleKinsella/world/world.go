d264e0a5bdcdcd57a2ad9a7371b31daff55bc01c
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2024-11-09 23:23:36 +0000

    added colours to sharks, fishes and empty water and populate 2d-array with random value either being a shark, fish or empty water.
    I have added a colour to my sharks, fishes and empty water. A shark is red, a fish is blue and empty water is black.
    
    Then I iterate over the data array to get a random value, then I populate my 2d-array with the random value that is retrieved.

--- Wa-tor KyleKinsella/world/world.go
+++ Wa-tor KyleKinsella/world/world.go
@@ -5,15 +5,32 @@ import (
 	"math/rand"
 )
 
+const (
+	Reset   = "\033[0m"
+	Red     = "\033[31m"
+	Blue    = "\033[34m"
+	Black   = "\033[30m"
+)
+
+func gameMessage() {
+	// Adding colors to strings with ANSI codes
+	welcome := "\033[32m--------------------------Welcome to my Wa-tor Simulation project--------------------------" + Reset
+	shark := "\nA shark is in the color Red and denoted with the letter " + Red + "S" + Reset + "."
+	fish := "\nA fish is in the color Blue and denoted with the letter " + Blue + "F" + Reset + "."
+	emptyWater := "\nThe empty water is in the color Black and denoted with the letter " + Black + "EW" + Reset + "."
+	end := "\033[32m\n-------------------------------------------------------------------------------------------" + Reset
+	fmt.Println(welcome, shark, fish, emptyWater, end)
+}
+
 func main() {
 	rows := 30
-	cols := 68
+	cols := 60
 	grid := make([][]string, rows)	
 	for i := range grid { // 0-29
 		grid[i] = make([]string, cols) // Create a slice for each row
 	}
 
-	data := []string{"S", "F", "EW"}
+	data := []string{Red + "S" + Reset, Blue + "F" + Reset, Black + "EW" + Reset}	
 	// this code puts a random value into the 2d-array
 	for _, value := range data {
 		place := false
@@ -33,11 +50,16 @@ func main() {
 	for i:=0; i<rows; i++ {
 		for j:=0; j<cols; j++ {
 			if grid[i][j] == "" {
-				grid[i][j] = "_"
+				for i:=0; i<30; i++ {
+					randomIn := rand.Intn(len(data))
+					randomVal := data[randomIn]
+					grid[i][j] = randomVal
+				}
 			}
 		}
 	}
 	
+	gameMessage()
 	for i := range len(grid) {
 		fmt.Println(grid[i])
 	}
